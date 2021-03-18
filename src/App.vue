545e1d50d873da164288935e1678bb44707cd3e0
R8P <40828591+R8P@users.noreply.github.com>
2021-03-18 22:43:13 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/layout/Header'
+import Todos from '@/components/Todos'
+import AddTodo from '@/components/AddTodo'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  components: {
+    AddTodo,
+    Header,
+    Todos
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo (id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        // eslint-disable-next-line no-return-assign
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err))
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo (newTodo) {
+      const { title, completed } = newTodo
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        // eslint-disable-next-line no-return-assign
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err))
+    }
+  },
+  created () {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      // eslint-disable-next-line no-return-assign
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err))
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
